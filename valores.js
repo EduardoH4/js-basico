af656cec4b9d88f4e47250afc5ece354703ec354
Melvin Hernandez <melvineduardo4@gmail.com>
2023-05-22 20:10:40 -0600

    Tipo de Dato/valores en JS

---
+++ valores.js
@@ -0,0 +1,23 @@
+//TIPO DE DATOS/VALORES PRIMITIVOS
+
+   //Tipo de Dato/Valor Number
+40
+
+   //Tipo de Dato/Valor String
+"Melvin Hernandez"
+
+   //Tipo de Dato/Valor Boolean
+true
+false
+ 
+   //Otros Tipos de Dato/Valor
+null
+undefined
+
+//TIPO DE DATOS/VALORES OBJETO 
+
+   //Tipo de Dato/Valor Array
+[1,2,3]
+
+   //Tipo de Dato/Valor Objeto
+{nombre: "Melvin Hernandez"}
