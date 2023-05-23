a7cfd897d7113792cf632a16650c9bdee050af3b
Melvin Hernandez <melvineduardo4@gmail.com>
2023-05-22 23:36:55 -0600

    Variables en JS

---
+++ variables.js
@@ -0,0 +1,17 @@
+// Sintaxis de una Variable
+let nombre = "Melvin Hernandez";
+
+// Declarar una Variable
+let edad;
+
+// Inicializar una Variable 
+edad = 24;
+
+//variable tipo Array
+let elementos = ["Computadora", "Celular"];
+
+//variable tipo Objeto
+let persona = {
+    nombre: "Melvin",
+    edad: 24
+};
